1e68e7e315c2bfcd0cd72569b5580205ab69c344
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-09-29 19:11:07 +0200

    chore(global): added components, reset styles.

---
+++ src/components/App/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Header from "./../Header/Header.svelte";
+</script>
+
+<main class="content">
+  <Header />
+
+  <div class="content__box">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam ducimus nulla totam inventore, repellat
+      consequuntur deleniti, veritatis dolorum nesciunt eius repudiandae molestias ipsa numquam deserunt libero, quae
+      placeat quaerat. Doloribus?
+    </p>
+  </div>
+</main>
+
+<style>
+  .content {
+    display: flex;
+    flex-direction: column;
+
+    height: 100vh;
+
+    background-color: #f0fafb;
+  }
+
+  .content__box {
+    display: flex;
+    justify-content: center;
+
+    max-width: 1200px;
+
+    margin: 0 auto;
+    margin-top: 50px;
+  }
+</style>
---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<header class="header">
+  <h1 class="header__title">Jobs list</h1>
+</header>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: center;
+
+    padding: 25px 0;
+
+    background: #5da4a4;
+  }
+
+  .header__title {
+    display: flex;
+
+    font-size: 32px;
+    line-height: 36px;
+    font-family: "Roboto", sans-serif;
+    font-weight: 500;
+    text-align: center;
+
+    text-shadow: 2px 2px #000000;
+
+    color: #ffffff;
+  }
+</style>
